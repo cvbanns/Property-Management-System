9aba37160a9a53863e33c0b5b5bc4564131b969b
Caleb Aregbesola <calebaregbesola@gmail.com>
2022-12-20 20:11:53 +0100

    Generic Paginator Class

---
+++ HotelManagement.Application/Utilities/GenericPaginator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManagement.Application.Utility
+{
+    public class GenericPaginator<T> : List<T>
+    {
+        public int CurrentPage { get; private set; }
+        public int TotalPages { get; private set; }
+        public int PageSize { get; private set; }
+        public int TotalCount { get; private set; }
+        public bool HasPrevious => CurrentPage > 1;
+        public bool HasNext => CurrentPage < TotalPages;
+        public GenericPaginator(List<T> currentPageItems, int count, int pageNumber, int pageSize)
+        {
+            TotalCount = count;
+            PageSize = pageSize;
+            CurrentPage = pageNumber;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            AddRange(currentPageItems);
+        }
+        public static GenericPaginator<T> ToPagedList(IQueryable<T> source, int pageNumber, int pageSize)
+        {
+            var count = source.Count();
+            var currentPageItems = source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
+            return new GenericPaginator<T>(currentPageItems, count, pageNumber, pageSize);
+        }
+    }
+}
