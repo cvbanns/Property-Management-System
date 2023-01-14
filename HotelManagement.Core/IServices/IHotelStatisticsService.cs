970cffafc1128aaac8055f28e65216d858599c2c
Ifeanyi-M <gothikstormborn@gmail.com>
2023-01-14 10:34:37 +0100

    Sync with remote

--- HotelManagement.Core/IServices/IHotelStatisticsService.cs
+++ HotelManagement.Core/IServices/IHotelStatisticsService.cs
@@ -9,11 +9,11 @@ namespace HotelManagement.Core.IServices
     public interface IHotelStatisticsService
     {
         Task<Response<string>> GetTotalNumberOfHotels();
-        // Task<int> GetTotalNumberOfRooms(Hotel hotel);
+       
         Task<Response<string>> GetTotalNumberOfRooms(string Id);
-        // Task<int> GetTotalNumberOfRoomsOccupied(Hotel hotel);
+       
         Task<Response<string>> GetTotalNumberOfRoomsOccupied(string Id);
-        // Task<int> GetTotalNumberOfRoomsUnoccupied(Hotel hotel);
+        
         Task<Response<string>> GetTotalNumberOfRoomsUnoccupied(string Id);
     }
 }
