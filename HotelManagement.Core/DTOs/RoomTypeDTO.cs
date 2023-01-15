d5a5bebc19916303ff7904a45417ca0817ab38df
obyte1 <saintobite501@gmail.com>
2023-01-15 16:06:51 +0100

    RoomType dto added

---
+++ HotelManagement.Core/DTOs/RoomTypeDTO.cs
@@ -0,0 +1,24 @@
+﻿using HotelManagement.Core.Domains;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManagement.Core.DTOs
+{
+    public class RoomTypeDTO 
+    {
+        public string id { get; set; } = Guid.NewGuid().ToString();
+        public string Name { get; set; }       
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public decimal Discount { get; set; }
+        public string Thumbnail { get; set; }        
+        public int Available { get; set; }
+        public DateTime CreateAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        
+    }
+}
