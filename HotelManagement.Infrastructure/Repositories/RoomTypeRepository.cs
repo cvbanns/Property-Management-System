0afe2c54c46f9b5972aa11b509b78474520f8235
obyte1 <saintobite501@gmail.com>
2023-01-15 22:56:10 +0100

    Conflict resolved

---
+++ HotelManagement.Infrastructure/Repositories/RoomTypeRepository.cs
@@ -0,0 +1,24 @@
+﻿using HotelManagement.Core.Domains;
+using HotelManagement.Core.IRepositories;
+using HotelManagement.Infrastructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManagement.Infrastructure.Repositories
+{
+    public class RoomTypeRepository : GenericRepository<RoomType>, IRoomTypeRepository
+    {
+        public RoomTypeRepository(HotelDbContext hotelDbContext) : base(hotelDbContext)
+        {
+
+        }
+        public async void AddRoomType(string Hotel_Id, RoomType roomType)
+        {
+            roomType.HotelId = Hotel_Id;
+            await AddAsync(roomType);
+        }
+    }
+}
